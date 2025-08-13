bc70ec66d17edf149abcdc6953b511a34a59e6f3
Mohamed Rifat <mohamed.rifat100@gmail.com>
2025-08-13 03:07:01 +0300

    Fix cafes in mobile

--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -167,7 +167,7 @@ const Navbar = () => {
                   Products
                 </Link>
                 <Link
-                  to="/cafe"
+                  to="/cafes"
                   onClick={() => setIsOpen(false)}
                   className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-[#CF848A] hover:bg-gray-100"
                 >
